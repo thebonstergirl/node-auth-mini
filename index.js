eb6098af725731f4fe7d6c1beb03b8717ce5aba6
Bonnie Jordan <thebonstergirl@gmail.com>
2018-09-05 13:10:41 -0500

    finished

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+require('dotenv').config()
 const express = require('express');
 const session = require('express-session');
-
+const passport = require('passport');
+const strategy = require('./strategy');
 const app = express();
 
-const port = 3000;
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user, done)=> {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done)=>{
+    done(null, user);
+});
+
+app.get('/login', passport.authenticate('auth0', {
+    successRedirect: '/me',
+    failureRedirect: '/login'
+}))
+
+app.get('/me', (req, res, next)=> {
+    if (req.user) {
+        res.status(200).send(req.user);
+    }
+    else {res.status(401).send('No user');
+    }
+});
+
+const port = 3001;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
---
+++ strategy.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const Auth0Strategy = require('passport-auth0');
+
+module.exports = new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL:  '/login',
+    scope: 'openid email profile'
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      // accessToken is the token to call Auth0 API (not needed in the most cases)
+      // extraParams.id_token has the JSON Web Token
+      // profile has all the information from the user
+      return done(null, profile);
+    }
+  );
